b6a6c38563c6ef28abdff4db25204ca9d7740b7f
dennis mbithi <denmbithi@gmail.com>
2022-02-19 13:12:31 +0300

    added solution for  pangram

---
+++ pangram/pangram.py
@@ -0,0 +1,8 @@
+def is_pangram(sentence):
+    alpha="abcdefghijklmnopqrstuvwxyz"
+    sentence = "".join(sorted(set(list(sentence.lower()))))
+
+    if alpha in sentence:
+        return(True)
+    else:
+        return(False)

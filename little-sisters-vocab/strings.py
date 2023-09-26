355d13725ebc1eb34bfa9c19ba8fcbffc97452a4
Dennis Mbithi <denmbithi@gmail.com>
2023-09-26 16:24:11 +0300

    Adde little sisters vocal solution

---
+++ little-sisters-vocab/strings.py
@@ -0,0 +1,71 @@
+"""Functions for creating, transforming, and adding prefixes to strings."""
+
+
+def add_prefix_un(word):
+    """Take the given word and add the 'un' prefix.
+
+    :param word: str - containing the root word.
+    :return: str - of root word prepended with 'un'.
+    """
+
+    word = 'un'+word
+
+    return word
+
+
+def make_word_groups(vocab_words):
+    """Transform a list containing a prefix and words into a string with the prefix followed by the words with prefix prepended.
+
+    :param vocab_words: list - of vocabulary words with prefix in first index.
+    :return: str - of prefix followed by vocabulary words with
+            prefix applied.
+
+    This function takes a `vocab_words` list and returns a string
+    with the prefix and the words with prefix applied, separated
+     by ' :: '.
+
+    For example: list('en', 'close', 'joy', 'lighten'),
+    produces the following string: 'en :: enclose :: enjoy :: enlighten'.
+    """
+
+    pw = [vocab_words[0]+w for w in vocab_words[1:]]
+    pw.insert(0, vocab_words[0])
+
+    return " :: ".join(pw)
+
+
+def remove_suffix_ness(word):
+    """Remove the suffix from the word while keeping spelling in mind.
+
+    :param word: str - of word to remove suffix from.
+    :return: str - of word with suffix removed & spelling adjusted.
+
+    For example: "heaviness" becomes "heavy", but "sadness" becomes "sad".
+    """
+
+    word = word.removesuffix('ness')
+
+    if word.endswith('i'):
+        word = word.removesuffix('i')+'y'
+
+    return word
+
+
+def adjective_to_verb(sentence, index):
+    """Change the adjective within the sentence to a verb.
+
+    :param sentence: str - that uses the word in sentence.
+    :param index: int - index of the word to remove and transform.
+    :return: str - word that changes the extracted adjective to a verb.
+
+    For example, ("It got dark as the sun set", 2) becomes "darken".
+    """
+    sentence_list = sentence.split(' ')
+    
+    if sentence_list[index].endswith('.'):
+        word = sentence_list[index].removesuffix('.')+'en'
+    
+    else:
+        word = sentence_list[index]+'en'
+
+    return word
